e56f2fe5218b1d7d4440db932af3a8d231209ee8
Chalieta Audreylia <chalietaudreylia@gmail.com>
2026-05-20 01:14:10 -0700

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        vector<int> dp = {1, 2};
+        int i = 3;
+        while (i <= n) {
+            int tmp = dp[1];
+            dp[1] = dp[0] + tmp;
+            dp[0] = tmp;
+            ++i;
+        }
+
+        return dp[1];
+    }
+};
