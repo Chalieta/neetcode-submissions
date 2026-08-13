7b6be392dbab4fa69a3dfe85baefca78436112e6
Chalieta Audreylia <chalietaudreylia@gmail.com>
2026-08-13 21:42:05 +0800

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int> numSet;
+
+        for (int num : nums) {
+            if (numSet.count(num)) {
+                return num;
+            }
+            numSet.insert(num);
+        }
+
+        return -1;
+    }
+};
