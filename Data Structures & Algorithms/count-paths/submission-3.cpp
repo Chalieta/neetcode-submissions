16caa46177437c3cc1eaddde06b4d62f8dd301b1
Chalieta Audreylia <chalietaudreylia@gmail.com>
2026-09-24 17:42:23 +0800

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> row(n, 1);
+
+        for (int i = m - 2; i >= 0; i--) {
+            vector<int> newRow(n, 1);
+            for (int j = n - 2; j >= 0; j--) {
+                newRow[j] = newRow[j + 1] + row[j];
+            }
+            row = newRow;
+        }
+
+        return row[0];
+    }
+};
